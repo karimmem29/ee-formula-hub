2f2f20ff7955cb216fa8c6ed25357f49ab43db07
Aidiid <aidiidmm@gmail.com>
2026-06-24 12:18:04 +0800

    add google search console verification

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
         <meta name="description" content="Free electrical engineering calculators and formula reference. Instant solvers for DC circuits, AC, filters, op-amps, transistors, power systems and more." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="sW7OKOkm63VY2_84HkYx8TUSYGkx3kbFDVCLMJ1ruR4" />
       </Head>
 
       <Navbar />
