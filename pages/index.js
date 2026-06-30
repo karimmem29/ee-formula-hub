b965105bbf5319f25bb669e2847f39669d1e76aa
Aidiid <aidiidmm@gmail.com>
2026-06-30 12:24:59 +0800

    add second google verification tag for custom domain

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="google-site-verification" content="sW7OKOkm63VY2_84HkYx8TUSYGkx3kbFDVCLMJ1ruR4" />
+        <meta name="google-site-verification" content="l1OpOMFtw6rDE9sdm8kGc6OwPO22Xh18jXdMLy7Xd6c" />
       </Head>
 
       <Navbar />
